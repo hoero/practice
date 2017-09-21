f6c0eadda53245e4058bb99d5cc65bd9e1e82ad3
Dionmy Olivares <elementalboy@hotmail.com>
2017-09-20 20:13:17 -0400

    Explanation of bind, another solution with closure, and some solutions using underscoreJS

--- js-understanding-the-weird-parts/d20-functional-programming.js
+++ js-understanding-the-weird-parts/d20-functional-programming.js
@@ -47,9 +47,43 @@ console.log(results4); // [false, false, true]
 var simpleCheckPassedLimit = function(limit) {
     return function(limit, item) {
         return item > limit;
-    }.bind(null, limit);
+    }.bind(null, limit); 
 };
 
 var results5 = mapForEach(values, simpleCheckPassedLimit(1));
 
+/**
+ * bind modifies function signature to be just function(item) before return. so it will look like this,
+ * function(item) {
+        return item > 1;
+    }; 
+ */
+
 console.log(results5); // [false, true, true]
+
+// Another solution with closure
+var closure = function(limit) {
+    return function(item) {
+        return item > limit;
+    };
+};
+
+var results6 = mapForEach(values, closure(3));
+
+console.log(results6); // [false, false, false]
+
+// ---------------------------------------------------------------------------------------------------------
+// Using underscoreJS: needs it to be able to work
+ 
+var results7 = _.map(values, function(item) { return item * 3; });
+console.log(results2); // [3, 6, 9]
+
+var results8 = _.filter([2, 3, 4, 5, 6, 7], function(item) { return item % 2 === 0; });
+console.log(results2); // [2, 4, 6]
+
+/**
+ * In the second one, I want to get back only items that have a remainder when divided by 2 of 0.
+ * So this should give me back a filter.
+ * It will filter out of this list and give me back a sub list.
+ * So I've essentially queried this long array and gotten back only items divisible by 2.
+ */
