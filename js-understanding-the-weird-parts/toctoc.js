16036ecbc196287fd436b364f631c9f37f368e7c
Dionmy Olivares <elementalboy@hotmail.com>
2017-10-04 19:44:28 -0400

    Framework development, ¡Toc, toc!: safe code structure

---
+++ js-understanding-the-weird-parts/toctoc.js
@@ -0,0 +1,31 @@
+// ---------------------------------------------------------------------------------------------------------
+/**
+ * tocToc (¡Toc, toc! onomatopeya para llamar a la puerta) -> greeting framework
+ * 
+ * Lets say we wanted to build a library or framework that helps us give greetings.
+ * Maybe for when I'm on a website and someone logs in I can show them a greeting after they logged in in the upper right corner.
+ * And maybe a I'd like to do it In different languages, depending on what language they choose, their preferred language in the app.
+ * 
+ * Official requirements:
+ * 
+ * • When given a first name, last name, and optional language, it generates formal and informal greetings.
+ * 
+ * • Support English and Spanish languages.
+ * 
+ * • It should be reusable, meaning that it wont interfere with any other JS code in my app, and someone else can just grab it and use it in their apps.
+ * 
+ * • And even is called tocToc I'd like an easy type structure, like 'tt()'.
+ * 
+ * • All my developers use jQuery on a lot of their projects, so it should also support jQuery.
+ *   Even though it returns greeting what we'd like is to be able to give it a jQuery object that points at some HTML element,
+ *   and it'll fill that element with the greeting. 
+ * 
+ *   So I could just pass my tocToc, let's say a div or a span in my HTML that just contains text.
+ *   And it'll fill that with my appropriate greeting text.
+ */
+
+(function(global, obj) {
+
+    
+    
+}(window, jQuery));
