925c07ffa8a68bd74e87539a0b2d9c2d1a594efb
Dionmy Olivares <elementalboy@hotmail.com>
2017-10-05 20:11:23 -0400

    Framework development, ¡Toc, toc! : final touches and usage of it

--- js-understanding-the-weird-parts/toctoc.js
+++ js-understanding-the-weird-parts/toctoc.js
@@ -1,8 +1,8 @@
 // ---------------------------------------------------------------------------------------------------------
 /**
- * tocToc (¡Toc, toc! onomatopeya para llamar a la puerta) -> greeting framework
+ * tocToc (¡Toc, toc! onomatopeya para llamar a la puerta) -> greeting framework/library
  * 
- * Lets say we wanted to build a library or framework that helps us give greetings.
+ * Lets say we wanted to build a library or framework/library that helps us give greetings.
  * Maybe for when I'm on a website and someone logs in I can show them a greeting after they logged in in the upper right corner.
  * And maybe a I'd like to do it In different languages, depending on what language they choose, their preferred language in the app.
  * 
@@ -24,12 +24,12 @@
  *   And it'll fill that with my appropriate greeting text.
  */
 
-(function(global, $) {
+(function( global, $ ) {
 
     // Sets up the tocToc object and returns it, making a 'new' object
-    const tocToc = function(firsrtName, lastName, language) {
+    const tocToc = function( firsrtName, lastName, language ) {
  
-        return new tocToc.init(firsrtName, lastName, language);
+        return new Instance( firsrtName, lastName, language );
         
     };
 
@@ -41,41 +41,49 @@
           // Logger messages
           logMessages     = { en: 'Logged in', es: 'Inició Sesión' };
     
-    // tocToc prototype, holds methods (to save memory space)
-    tocToc.prototype = {
+    // The actual object is created here,
+    // allowing us to 'new' an object without calling 'new'
+    class Instance {
+        constructor( firsrtName = '', lastName = '', language = 'en' ) {
+            this.firsrtName = firsrtName;
+            this.lastName   = lastName;
+            this.language   = language;
+            
+            this.validate();
+        }
 
-        fullName: function() {
+        fullName() {
             // 'this' refers to the calling object at execution time
             return `${this.firsrtName} ${this.lastName}`;
-        },
+        }
 
         // Checks that is a valid language
         // References the externally inaccessible 'supportedLangs' within the closure
-        validate: function() {
+        validate() {
             if (supportedLangs.indexOf(this.language) === -1) {
                 throw 'Invalid language.';
             }
-        },
+        }
 
         // Retrieve messages from object by referring to properties using [] syntax
-        greeting: function() {
+        greeting() {
             return `${greetings[this.language]} ${this.firsrtName}!`;
-        },
+        }
 
-        formalGreeting: function(params) {
+        formalGreeting() {
             return `${formalGreetings[this.language]}, ${this.fullName()}`;
-        },
+        }
 
         // Determines the kind of message
-        isFormal: function(isIt) {
+        isFormal(isIt) {
             // if undefined or null, it will be coerced to 'false'
             let greeting = (isIt) ? this.formalGreeting() : this.greeting();
 
             return greeting;
-        },
+        }
 
         // Chainable methods return their own containing object
-        greet: function(formal) {
+        greet(formal) {
             var msg = this.isFormal(formal);
 
             // It logs it to the console. IE actually doesnt have a console variable unless its console is open.
@@ -88,20 +96,20 @@
             // 'this' refers to the calling object at execution time
             // Makes the method chainable
             return this;
-        },
+        }
 
         // Manually make sure something is logged
-        log: function() {
+        log() {
             if (global.console) {
                 console.log(`${logMessages[this.language]}: ${this.fullName()}`);
             }
 
             // Make chainable
             return this;
-        },
+        }
 
         // Changes the language on the fly
-        setLanguage: function(lang) {
+        setLanguage(lang) {
             // Sets the language
             this.language = lang;
 
@@ -110,11 +118,11 @@
 
             // Make chainable
             return this;
-        },
+        }
         
         // This uses a selector, whether or not it's formal and sets up the greeting itself,
         // and updates whatever value is there in the DOM
-        DOMgreet: function(selector, formal) {
+        DOMgreet( selector, formal ) {
             if (!$) {
                 throw 'jQuery not loaded';
             }
@@ -132,28 +140,11 @@
             // Make chainable
             return this;
         }
-
-    };
-
-    // tocToc function constructor, the actual object is created here,
-    // allowing us to 'new' an object without calling 'new'
-    tocToc.init = function(firsrtName = '', lastName = '', language = 'en') {
-
-        this.firsrtName = firsrtName;
-        this.lastName   = lastName;
-        this.language   = language;
-        
-        this.validate();
-
-    };
-
-    // Sets init protorype to be tocToc's, trick borrowed from jQuery
-    // so we don't have to use the 'new' keyword
-    tocToc.init.prototype = tocToc.prototype;
+    }
 
     // Exposes tocToc and the alias 'tt' to the global environment,
     // attaching our Greetr to the global object,
     // and provide a shorthand 'tt' for ease our poor fingers
     global.tocToc = global.tt = tocToc;
     
-}(window, jQuery));
+}( window, jQuery ));
