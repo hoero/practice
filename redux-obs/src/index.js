d7e6b3a70de162567cf80e5f57d45eb48dc5f59b
Dionmy Olivares <elementalboy@hotmail.com>
2017-10-10 14:06:02 -0400

    Adding redux-observable to an existing Redux project

--- redux-obs/src/index.js
+++ redux-obs/src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import reducer from './reducers';
-import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(reducer);
+import { createEpicMiddleware } from 'redux-observable';
+import { rootEpic } from "./epics/index";
+
+const epicMiddleware = createEpicMiddleware(rootEpic);
+
+const store = createStore(reducer, applyMiddleware(epicMiddleware));
 
 ReactDOM.render(
     <Provider store={store}>
         <App />
     </Provider>
     , document.getElementById('root'));
-registerServiceWorker();
