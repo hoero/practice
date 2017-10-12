6233678dda30f86bab1307d2f1b6ed836d9962ce
Dionmy Olivares <elementalboy@hotmail.com>
2017-10-12 11:46:16 -0400

    Debounce user input to avoid repeated Ajax requests

--- redux-obs/src/App.js
+++ redux-obs/src/App.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux";
 import './App.css';
-import Users from './components/Users';
+import { Beers } from './components/Beers';
+import { Search } from './components/Search';
+import { searchBeers } from './actions/index3';
 
 class App extends Component {
+  handleBeerSearch = (query) => {
+    this.props.searchBeers(query);
+  };
+
   render() {
     return (
       <div className="App">
-        <Users />
+        <Search
+          defaultValue = {''}
+          onChange     = {this.handleBeerSearch}
+        />
+
+        <Beers
+          beers   = {this.props.beers}
+          loading = {this.props.loading}
+        />
       </div>
     );
   }
 }
 
-export default App;
+export default connect((state) => state, {searchBeers})(App);
