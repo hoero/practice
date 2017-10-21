c5bdc922ae0817f787bb0193e461478f590a6dd2
Dionmy Olivares <elementalboy@hotmail.com>
2017-10-21 13:02:10 -0400

    Object literal extensions + ESLINT

--- accelerated-es6-training/src/index.js
+++ accelerated-es6-training/src/index.js
@@ -1,3 +1,6 @@
+//  -------------------------------------------------------------------------
+//region Arrow Functions
+
 const btn = document.querySelector('button');
 
 const afn = () => console.log(this);
@@ -11,3 +14,24 @@ afn(); // Window object
 
 btn.addEventListener('click', fn);  // HTML button element
 btn.addEventListener('click', afn); // Window object
+
+//endregion -------------------------------------------------------------------------
+//region Object literal extensions
+
+let name     = `Ana`;
+let age      = 25;
+let ageField = "age";
+
+let obj  = {
+    "name": `Max`,
+    [ageField]: 28,
+    "greet me"() {
+        console.log(`${this.name}, ${this.age}`); // Max, 25
+    }
+};
+
+obj["greet me"]();
+
+console.log(obj.age);       // 28
+console.log(obj['age']);    // 28
+console.log(obj[ageField]); // 28
