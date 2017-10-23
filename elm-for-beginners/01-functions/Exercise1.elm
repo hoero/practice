fbc4032480684deb29fd3af17447e42b9b10ce56
Dionmy Olivares <elementalboy@hotmail.com>
2017-10-23 17:56:50 -0400

    Functions: Exercise 1

---
+++ elm-for-beginners/01-functions/Exercise1.elm
@@ -0,0 +1,13 @@
+-- Exercise 1
+-- 
+-- 1. Create a new file named Exercise1.elm
+-- 2. Install the Html module
+-- 3. Import the Html Module into the Exercise1.elm file
+-- 4. Show your name on the page
+
+module Main exposing (..)
+
+import Html
+
+main =
+    Html.text "Hoeru"
