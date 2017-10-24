98d62d1f2729cd0db99aa1784f452dae3b5e8f64
Dionmy Olivares <elementalboy@hotmail.com>
2017-10-24 17:48:31 -0400

    Infix and Prefix functions: Exercise 2

---
+++ elm-for-beginners/02-functions++/Exercise2.elm
@@ -0,0 +1,20 @@
+-- Exercise 2
+-- 
+-- 1. Call your new function ~= as a prefix function.
+-- 2. Display the result on the page
+
+module Main exposing (..)
+
+import Html
+import String
+
+(~=) str1 str2 =
+    if String.left 1 str1 == String.left 1 str2 then
+        True
+    else
+        False
+
+main =
+    (~=) "Hoeru" "Oriba"
+        |> toString
+        |> Html.text
