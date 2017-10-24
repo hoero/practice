e6d51ec08ff10b717481078a9fcea8281e877fc7
Dionmy Olivares <elementalboy@hotmail.com>
2017-10-24 12:39:01 -0400

    Classes: getters and setters

--- accelerated-es6-training/src/index1.js
+++ accelerated-es6-training/src/index1.js
@@ -13,3 +13,41 @@ console.log(imported.keyValue); // 2000
 console.log(imported.default); // Some text
 
 //endregion -------------------------------------------------------------------------
+//region Classes
+class Person {
+    constructor (name) {
+        this._name = name;
+    }
+    
+    // Getter
+    get name() {
+        return this._name.toUpperCase();
+    }
+
+    // Setter
+    set name(value) {
+        if (value.length > 2) {
+
+            this._name = value;
+
+        } else {
+
+            console.log('Rejected');
+            
+        }
+    }
+}
+
+let person = new Person('Max');
+
+console.log(person); // Person {_name: "Max"}
+console.log(person.name); // MAX
+
+person.name = 'Ma' // Rejected
+
+console.log(person); // Person {_name: "Max"}
+
+person.name = 'Anna'
+
+console.log(person); // Person {_name: "Anna"}
+console.log(person.name); // ANNA
