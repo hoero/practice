bd5c2f9566114ffceb1f276f91dcafd2271d9344
Dionmy Olivares <elementalboy@hotmail.com>
2017-10-25 11:08:36 -0400

    Shared and unique Symbols

--- accelerated-es6-training/src/index2.js
+++ accelerated-es6-training/src/index2.js
@@ -16,3 +16,38 @@ let obj = {
 };
 
 console.log(obj); // {name: "Max", Symbol(debug): 22}
+
+//endregion -------------------------------------------------------------------------
+//region Shared Symbols
+
+let sharedSymbol  = Symbol.for('age');
+let sharedSymbol2 = Symbol.for('age');
+
+console.log(sharedSymbol === sharedSymbol2); // true
+
+let person = {
+    name: 'Max'
+};
+
+function makeAge(person) {
+    let ageSymbol = Symbol.for('age');
+
+    person[ageSymbol] = 27;
+}
+
+makeAge(person);
+
+console.log(person[sharedSymbol]); // 27
+
+// With unique IDs
+symbol = Symbol('age');
+
+function makeAgeUnique(person) {
+    let ageSymbol = Symbol('age');
+
+    person[ageSymbol] = 27;
+}
+
+makeAgeUnique(person);
+
+console.log(person[symbol]); // undefined
