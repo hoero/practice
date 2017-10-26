53ab56234a0d73791483d80985d802b0f6c73e4d
Dionmy Olivares <elementalboy@hotmail.com>
2017-10-26 11:09:26 -0400

    Iterators in Action

--- accelerated-es6-training/src/index3.js
+++ accelerated-es6-training/src/index3.js
@@ -13,3 +13,27 @@ console.log(it.next()); // {value: 1, done: false}
 console.log(it.next()); // {value: 2, done: false}
 console.log(it.next()); // {value: 3, done: false}
 console.log(it.next()); // {value: undefined, done: true}
+
+//endregion -------------------------------------------------------------------------
+//region Iterators in Action
+
+array[Symbol.iterator] = function() {
+
+    let nextValue = 10;
+    
+    return {
+        next() {
+            nextValue++;
+
+            return {
+                done : nextValue > 15 ? true : false,
+                value: nextValue
+            }
+        }
+    }
+
+}
+
+for (let element of array) {
+    console.log(element); // 11 12 13 14 15
+}
