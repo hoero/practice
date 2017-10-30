9a42efe6e75765ffe09bd4c5ada4e0f410d40f9b
Dionmy Olivares <elementalboy@hotmail.com>
2017-10-30 11:18:49 -0400

    Rejecting Promises

--- accelerated-es6-training/src/index4.js
+++ accelerated-es6-training/src/index4.js
@@ -1,10 +1,21 @@
 // -------------------------------------------------------------------------
 //region Creating & Resolving Promises
 
-let promise = new Promise((resolve, reject) => {
+let promise = new Promise(( resolve, reject ) => {
     setTimeout(() => {
         resolve('Done');
     }, 1500);
 });
 
 promise.then(value => console.log(value) ); // Done
+
+//endregion -------------------------------------------------------------------------
+//region Rejecting Promises
+
+promise = new Promise(( resolve, reject ) => {
+    setTimeout(() => {
+        reject('Failed!!!');
+    }, 1500);
+});
+
+promise.then( value => console.log(value), error => console.log(error) );  // Failed!!!
