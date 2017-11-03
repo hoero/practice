5de10641f7b040a4423499ceab6817907b4e9c43
Dionmy Olivares <elementalboy@hotmail.com>
2017-11-03 10:27:41 -0400

    Sets

--- accelerated-es6-training/src/index6.js
+++ accelerated-es6-training/src/index6.js
@@ -18,3 +18,24 @@ deck.set(key2, cardKing);
 
 console.log(deck.get(key1)); // {name: "Ace of Spades"}
 console.log(deck.get(key2)); // {name: "King of Clubs"}
+
+//endregion -------------------------------------------------------------------------
+//region Sets
+
+let set = new Set([1, 1, 1]);
+
+set.add(2);
+
+for (let key of set) { console.log(key); } // 1 2
+
+console.log(set.has(2)); // true
+
+for (let key of set.entries()) { console.log(key); } // [1, 1] [2, 2]
+for (let key of set.keys()) { console.log(key); }    // 1 2
+for (let key of set.values()) { console.log(key); }  // 1 2
+
+set.delete(1);
+
+for (let key of set) { console.log(key); } // 2
+
+set.clear();
