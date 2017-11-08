6251542cc65a4a4eab99e97f7edc5b2e76058b78
Dionmy Olivares <elementalboy@hotmail.com>
2017-11-08 12:31:33 -0400

    Calling Functions with Reflect.apply()

--- accelerated-es6-training/src/index7.js
+++ accelerated-es6-training/src/index7.js
@@ -1,5 +1,5 @@
 // -------------------------------------------------------------------------
-//region The Reflect API
+//region The Reflect API: Creating Objects with Reflect.construct()
 
 class Person {
     constructor(name) {
@@ -20,3 +20,24 @@ person = Reflect.construct(Person, ['Max'], newPrototype);
 
 console.log(person.__proto__ === Person.prototype);       // false
 console.log(person.__proto__ === newPrototype.prototype); // true
+
+//endregion -------------------------------------------------------------------------
+//region Calling Functions with Reflect.apply()
+
+class Person1 {
+    constructor(name, age) {
+        this.name = name;
+        this.age  = age;
+    }
+
+    greet(prefix) {
+        console.log(`${prefix} Hello, I am ${this.name}`);
+    }
+}
+
+person = Reflect.construct(Person1, ['Max', 27]);
+
+Reflect.apply(person.greet, person, []);           // Hello, I am Max
+Reflect.apply(person.greet, person, ['...']);      // ... Hello, I am Max
+Reflect.apply(person.greet, {}, []);               // Hello, I am undefined
+Reflect.apply(person.greet, { name: 'Anna' }, []); // Hello, I am Anna
