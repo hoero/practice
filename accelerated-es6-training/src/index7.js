79a9384fbd68215935d4935c28f373c6ed39e333
Dionmy Olivares <elementalboy@hotmail.com>
2017-11-08 15:30:22 -0400

    Reflect and Prototypes

--- accelerated-es6-training/src/index7.js
+++ accelerated-es6-training/src/index7.js
@@ -41,3 +41,28 @@ Reflect.apply(person.greet, person, []);           // Hello, I am Max
 Reflect.apply(person.greet, person, ['...']);      // ... Hello, I am Max
 Reflect.apply(person.greet, {}, []);               // Hello, I am undefined
 Reflect.apply(person.greet, { name: 'Anna' }, []); // Hello, I am Anna
+
+//endregion -------------------------------------------------------------------------
+//region Reflect and Prototypes
+
+class Person2 {
+    constructor() {
+        this.name = 'Max';
+    }
+}
+
+person = new Person();
+
+Person.prototype.age = 27;
+
+console.log(Reflect.getPrototypeOf(person)); // {age: 27, constructor: ƒ}
+
+console.log(Reflect.getPrototypeOf(person) === Person.prototype); // true
+
+let proto = {
+    age: 30
+};
+
+Reflect.setPrototypeOf(person, proto);
+
+console.log(Reflect.getPrototypeOf(person)); // {age: 30}
