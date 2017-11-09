869da35057d6dda0a879ec5119f6cf910d663277
Dionmy Olivares <elementalboy@hotmail.com>
2017-11-09 13:02:42 -0400

    Proxies and Reflect

--- accelerated-es6-training/src/index8.js
+++ accelerated-es6-training/src/index8.js
@@ -15,3 +15,32 @@ let proxy = new Proxy(person, handler);
 
 console.log(proxy.age);  // 27
 console.log(proxy.name); // Non existant
+
+//endregion -------------------------------------------------------------------------
+//region Proxies and Reflect
+
+person = {
+    age : 27,
+    name: 'Max'
+};
+
+handler = {
+    get(target, name) {
+        return name in target ? target[name] : 'Non existant';
+    },
+    set(target, property, value) {
+        if (value.length >= 2) {
+            Reflect.set(...arguments);
+        }
+    }
+};
+
+proxy = new Proxy(person, handler);
+
+console.log(proxy.name); // Max
+
+proxy.name = 'M';
+console.log(proxy.name); // Max
+
+proxy.name = 'Maximilian';
+console.log(proxy.name); // Maximilian
