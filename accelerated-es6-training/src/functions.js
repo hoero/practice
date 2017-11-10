cdf9e4682a76befbb1ed0d1639184f8161e4c223
Dionmy Olivares <elementalboy@hotmail.com>
2017-11-10 11:49:23 -0400

    Sending the Http Request

--- accelerated-es6-training/src/functions.js
+++ accelerated-es6-training/src/functions.js
@@ -1,8 +1,10 @@
 import * as DOMe from './elements'
+import { Http } from './http';
 
 function searchWeather() {
 
     const CITY_NAME = DOMe.ELEMENT_SEARCHED_CITY.value.trim();
+    const URL = `https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22${CITY_NAME}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`;
 
     if (CITY_NAME.length === 0) {
 
@@ -15,6 +17,12 @@ function searchWeather() {
         console.log(CITY_NAME);
 
     }
+
+    Http.fetchData(URL)
+        .then(responseData => {
+
+        })
+        .catch(error => alert(error));
 
 }
 
