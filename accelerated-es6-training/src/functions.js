7885e931d81355adc9f4d542a19bfa2e99c2800a
Dionmy Olivares <elementalboy@hotmail.com>
2017-11-10 11:29:16 -0400

    Fetching User Input

--- accelerated-es6-training/src/functions.js
+++ accelerated-es6-training/src/functions.js
@@ -1,5 +1,21 @@
+import * as DOMe from './elements'
+
 function searchWeather() {
-    console.log('Searching');
+
+    const CITY_NAME = DOMe.ELEMENT_SEARCHED_CITY.value.trim();
+
+    if (CITY_NAME.length === 0) {
+
+        DOMe.ELEMENT_SEARCH_BUTTON.disabled;
+
+    } else {
+
+        DOMe.ELEMENT_SEARCH_BUTTON.disabled = false;
+
+        console.log(CITY_NAME);
+
+    }
+
 }
 
 export { searchWeather };
