011da18553263f7e7238c50d46978ffafdd3308b
Dionmy Olivares <elementalboy@hotmail.com>
2017-11-10 14:01:21 -0400

    Displaying Data ++

--- accelerated-es6-training/src/elements.js
+++ accelerated-es6-training/src/elements.js
@@ -5,5 +5,5 @@ export const ELEMENT_LOADING_TEXT        = document.querySelector('#load');
 export const ELEMENT_WEATHER_BOX         = document.querySelector('#weather');
 
 export const ELEMENT_WEATHER_CITY        = ELEMENT_WEATHER_BOX.firstElementChild;
-export const ELEMENT_WEATHER_DESCRIPTION = document.querySelector('#weather');
+export const ELEMENT_WEATHER_DESCRIPTION = document.querySelector('#weatherDescription');
 export const ELEMENT_WEATHER_TEMPERATURE = ELEMENT_WEATHER_BOX.lastElementChild;
