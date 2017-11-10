9de579bfd1aae693db368f98ed338e873459df85
Dionmy Olivares <elementalboy@hotmail.com>
2017-11-10 13:08:32 -0400

    Fetching and Storing Weather Data

--- accelerated-es6-training/src/functions.js
+++ accelerated-es6-training/src/functions.js
@@ -1,10 +1,13 @@
 import * as DOMe from './elements'
 import { Http } from './http';
+import { WeatherData, WEATHER_PROXY_HANDLER } from './weather-data';
+import { escape } from 'querystring';
 
 function searchWeather() {
 
-    const CITY_NAME = DOMe.ELEMENT_SEARCHED_CITY.value.trim();
-    const URL = `https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22${CITY_NAME}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`;
+    const CITY_NAME = DOMe.ELEMENT_SEARCHED_CITY.value.trim(),
+          QUERY     = escape(`select item from weather.forecast where woeid in (select woeid from geo.places where text='${CITY_NAME}') and u='c'`),
+          URL       = `https://query.yahooapis.com/v1/public/yql?q=${QUERY}&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`;
 
     if (CITY_NAME.length === 0) {
 
@@ -20,6 +23,12 @@ function searchWeather() {
 
     Http.fetchData(URL)
         .then(responseData => {
+
+            const response      = responseData.query.results.channel.item.condititon,
+                  WEATHER_DATA  = new WeatherData(CITY_NAME, response.text.toUpperCase()),
+                  WEATHER_PROXY = new Proxy(WEATHER_DATA, WEATHER_PROXY_HANDLER);
+
+            WEATHER_PROXY.temperature = response.temp;
 
         })
         .catch(error => alert(error));
