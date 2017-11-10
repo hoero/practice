e8eb37d5e0bd7efe1ee0f26a181dc48621c676d0
Dionmy Olivares <elementalboy@hotmail.com>
2017-11-10 11:01:51 -0400

    Listening to User Events

--- accelerated-es6-training/src/app.js
+++ accelerated-es6-training/src/app.js
@@ -1,1 +1,6 @@
 const css = require('./app.css');
+
+import * as DOMe from './elements';
+import { searchWeather } from "./functions";
+
+DOMe.ELEMENT_SEARCH_BUTTON.addEventListener('click', searchWeather);
---
+++ accelerated-es6-training/src/functions.js
@@ -0,0 +1,5 @@
+function searchWeather() {
+    console.log('Searching');
+}
+
+export { searchWeather };
