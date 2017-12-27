153bf8481e48c4bb8cc4dc53062f328428fc2e33
Dionmy Olivares <elementalboy@hotmail.com>
2017-12-27 18:13:57 -0400

    Branch update

--- accelerated-es6-training/src/app.js
+++
@@ -1,6 +0,0 @@
-const css = require('./app.css');
-
-import * as DOMe from './elements';
-import { searchWeather } from "./functions";
-
-DOMe.ELEMENT_SEARCH_BUTTON.addEventListener('click', searchWeather);
