894b2d54b5c4ff1853dc94209d80be38f9fa2511
Dionmy Olivares <elementalboy@hotmail.com>
2017-12-27 18:27:51 -0400

    Brach update

--- js-understanding-the-weird-parts/c8-booleans-existence.js
+++
@@ -1,7 +0,0 @@
-var a, a = null, a = ''; // False is the Boolean of these
-
-if (a) { // This will attempt to convert to a Boolean whatever is inside the ().
-    console.log('Something is here');
-}
-
-// Nothing will happen because all those values are false.
