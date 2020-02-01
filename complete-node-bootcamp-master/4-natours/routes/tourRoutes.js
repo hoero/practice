d07de6622b766dc4abceaa5cd0774164e60d96b0
Dionmy Olivares <dionmy.olivares@adtechnacity.com>
2020-02-01 17:03:30 -0500

    Section 10: Authentication

--- complete-node-bootcamp-master/4-natours/routes/tourRoutes.js
+++ complete-node-bootcamp-master/4-natours/routes/tourRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const { specific } = require('../common/data');
 const tourController = require('../controllers/tourController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
@@ -17,13 +18,17 @@ router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
 router
     .route('/')
-    .get(tourController.getAllTours)
+    .get(authController.protect, tourController.getAllTours)
     .post(tourController.createTour);
 
 router
     .route(specific)
     .get(tourController.getTour)
     .patch(tourController.updateTour)
-    .delete(tourController.deleteTour);
+    .delete(
+        authController.protect,
+        authController.restrictTo('admin', 'lead-guide'),
+        tourController.deleteTour
+    );
 
 module.exports = router;
--- complete-node-bootcamp-master/4-natours/routes/userRoutes.js
+++ complete-node-bootcamp-master/4-natours/routes/userRoutes.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const { specific } = require('../common/data');
 const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
+
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
 
 router
     .route('/')
