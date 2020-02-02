ed9104736acfc6903c3c8cc58a253187d8e225fe
Dionmy Olivares <dionmy.olivares@adtechnacity.com>
2020-02-02 14:19:35 -0500

    Section 10: Authorization ++

--- complete-node-bootcamp-master/4-natours/models/userModel.js
+++ complete-node-bootcamp-master/4-natours/models/userModel.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
@@ -38,7 +39,14 @@ const userSchema = new mongoose.Schema({
             message: 'Passwords are not the same!'
         }
     },
-    passwordChangedAt: Date
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date,
+    active: {
+        type: Boolean,
+        default: true,
+        select: false
+    }
 });
 
 userSchema.pre('save', async function(next) {
@@ -50,6 +58,19 @@ userSchema.pre('save', async function(next) {
 
     // Delete passwordConfirm field
     this.passwordConfirm = undefined;
+    next();
+});
+
+userSchema.pre('save', function(next) {
+    if (!this.isModified('password') || this.isNew) return next();
+
+    this.passwordChangedAt = Date.now() - 1000;
+    next();
+});
+
+userSchema.pre(/^find/, function(next) {
+    // this points to the current query
+    this.find({ active: { $ne: false } });
     next();
 });
 
@@ -71,6 +92,19 @@ userSchema.methods.changePasswordAfter = function(JWTTimesStamp) {
     }
 
     return false;
+};
+
+userSchema.methods.createPasswordResetToken = function() {
+    const resetToken = crypto.randomBytes(32).toString('hex');
+
+    this.passwordResetToken = crypto
+        .createHash('sha256')
+        .update(resetToken)
+        .digest('hex');
+
+    this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+    return resetToken;
 };
 
 const User = mongoose.model('User', userSchema);
