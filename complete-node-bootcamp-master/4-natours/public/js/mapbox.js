95184794da5a2deed1f4b111dbae619f41c3d451
Dionmy Olivares <dionmy.olivares@adtechnacity.com>
2020-02-20 21:42:17 -0500

    Section 12: Map with MapBox

---
+++ complete-node-bootcamp-master/4-natours/public/js/mapbox.js
@@ -0,0 +1,46 @@
+console.log('Hello from the client');
+/* eslint-disable */
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
+
+mapboxgl.accessToken =
+    'pk.eyJ1IjoiaG9lcnUiLCJhIjoiY2s2dmplNWt3MDJqNDNsbWcya3VrbDh2byJ9.KbyINFYrFWoRmUp4XD1Ruw';
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/hoeru/ck6vjhoer02zg1imztp1107u5',
+    scrollZoom: false
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach(loc => {
+    // Create marker
+    const el = document.createElement('div');
+    el.className = 'marker';
+
+    // Add marker
+    new mapboxgl.Marker({
+        element: el,
+        anchor: 'bottom'
+    })
+        .setLngLat(loc.coordinates)
+        .addTo(map);
+
+    // Add popup
+    new mapboxgl.Popup({ offset: 30 })
+        .setLngLat(loc.coordinates)
+        .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+        .addTo(map);
+
+    // Extend map bounds to include current location
+    bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+    padding: {
+        top: 200,
+        bottom: 150,
+        left: 100,
+        right: 100
+    }
+});
