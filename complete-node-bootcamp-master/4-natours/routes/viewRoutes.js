cf8b59f435d163b4d9f83bb71a2237b12b1e87d0
Dionmy Olivares <dionmy.olivares@adtechnacity.com>
2020-02-28 14:40:30 -0500

    Section 13: Bookings

--- complete-node-bootcamp-master/4-natours/routes/viewRoutes.js
+++ complete-node-bootcamp-master/4-natours/routes/viewRoutes.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const viewsController = require('./../controllers/viewsController');
 const authController = require('./../controllers/authController');
+const bookingController = require('./../controllers/bookingController');
 
 const router = express.Router();
 
 router.get('/me', authController.protect, viewsController.getAccount);
+router.get('/my-tours', authController.protect, viewsController.getMyTours);
 
 router.use(authController.isLoggedIn);
 
-router.get('/', viewsController.getOverview);
+router.get(
+    '/',
+    bookingController.createBookingCheckout,
+    viewsController.getOverview
+);
 router.get('/tour/:slug', viewsController.getTour);
 router.get('/login', viewsController.getLoginForm);
 
