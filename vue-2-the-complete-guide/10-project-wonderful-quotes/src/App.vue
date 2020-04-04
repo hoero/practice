06c1365af481271312f1fd5149756d36bdf5c6d6
Dionmy Olivares <hoeru@outlook.com>
2020-04-04 15:21:24 -0400

    Section 10: Wonderful Quotes

---
+++ vue-2-the-complete-guide/10-project-wonderful-quotes/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container">
+        <appHeader :count="quotes.length" :max="max"></appHeader>
+        <newQuote @quoteAdded="newQuote"></newQuote>
+        <quoteGrid :quotes="quotes" @quoteDeleted="deleteQuote"></quoteGrid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">Info: Click on a Quote to delete it!</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import QuoteGrid from './components/QuoteGrid.vue';
+import NewQuote from './components/NewQuote.vue';
+import Header from './components/Header.vue';
+
+export default {
+    data() {
+        return {
+            quotes: ['Ut ad corporis ea repellendus dolor hic qui at.'],
+            max: 10
+        };
+    },
+    components: { QuoteGrid, NewQuote, appHeader: Header },
+    methods: {
+        newQuote(quote) {
+            if (this.quotes.length >= this.max)
+                return alert('Please delete a Quote first!');
+            this.quotes.push(quote);
+        },
+        deleteQuote(i) {
+            this.quotes.splice(i, 1);
+        }
+    }
+};
+</script>
+
+<style>
+</style>
---
+++ vue-2-the-complete-guide/10-project-wonderful-quotes/src/components/QuoteGrid.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="row">
+        <quote v-for="(quote, i) in quotes" :key="quote" @click.native="deleteQuote(i)">{{quote}}</quote>
+    </div>
+</template>
+
+<script>
+import Quote from './Quote';
+
+export default {
+    props: ['quotes'],
+    components: { Quote },
+    methods: {
+        deleteQuote(i) {
+            this.$emit('quoteDeleted', i);
+        }
+    }
+};
+</script>
+
+<style>
+</style>
