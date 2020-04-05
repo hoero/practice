f38d53e1f16623152fbca65662015da9feb203b6
Dionmy Olivares <hoeru@outlook.com>
2020-04-05 13:46:21 -0400

    Section 11: Handling user input with forms

---
+++ vue-2-the-complete-guide/11-forms/practice/src/components/Name.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="form-group">
+        <label>
+            <slot></slot>
+        </label>
+        <input
+            type="text"
+            class="form-control"
+            :value="name"
+            @change="changed($event.target.value)"
+        />
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['name'],
+    methods: {
+        changed(value) {
+            this.$emit('input', value);
+        }
+    }
+};
+</script>
